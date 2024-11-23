688024f50f27f81f923c3dcf4805854e56dd2059
Jason Wen <haibin.wen3@gmail.com>
2024-11-22 19:18:35 -0500

    fix

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -68,7 +68,7 @@ static void toyota_rx_hook(const CANPacket_t *to_push) {
     int addr = GET_ADDR(to_push);
 
     if (addr == 0x412) {
-      lkas_button = (uint8_t)(GET_BYTE(to_push, 0U) & 0xC0U) != 0;
+      lkas_button = (GET_BYTE(to_push, 0U) & 0xC0U) != 0;
       mads_check_lkas_button();
     }
   }
