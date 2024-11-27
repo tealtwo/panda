f12cad8b795969da1567579ed4f9ce26c763487b
Jason Wen <haibin.wen3@gmail.com>
2024-11-27 01:24:40 -0500

    Flash via CAN, but for real this time

--- board/escc/flash.py
+++ board/escc/flash.py
@@ -1,18 +1,43 @@
 #!/usr/bin/env python3
 import os
 import subprocess
-
-from panda import Panda
+import time
+import argparse
+from panda import Panda, CanHandle, McuType
 
 board_path = os.path.dirname(os.path.realpath(__file__))
 
+
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Flash ESCC over can')
+  parser.add_argument('--recover', action='store_true')
+  args = parser.parse_args()
+
   print(board_path)
   subprocess.check_call(f"scons -C {board_path}/../.. -j$(nproc) {board_path} --escc", shell=True)
 
-  serials = Panda.list()
-  print(f"found {len(serials)} panda(s) - {serials}")
-  for s in serials:
-    print("flashing", s)
-    with Panda(serial=s) as p:
-      p.flash("obj/escc.bin.signed")
+  p = Panda()
+  p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+
+  _mcu_type = p.get_mcu_type()
+  if _mcu_type == McuType.H7:
+    print("ESCC not supported on H7")
+    exit(0)
+
+  while 1:
+    if len(p.can_recv()) == 0:
+      break
+
+  if args.recover:
+    p.can_send(0x2AC, b"\xd1\x00", 0)
+    exit(0)
+  else:
+    p.can_send(0x2AC, b"\xd1\x01", 0)
+
+  time.sleep(0.1)
+  print("flashing ESCC")
+  with open("obj/escc.bin.signed", "rb") as f:
+    code = f.read()
+  Panda.flash_static(CanHandle(p, 0), code, mcu_type=_mcu_type)
+
+  print("can flash done")
