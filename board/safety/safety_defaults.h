dffd472529f1aa84a45df368710e0b26c77b9e9a
Jason Wen <haibin.wen3@gmail.com>
2025-03-13 04:23:21 -0400

    ESCC and SMDPS support in a single panda

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -1,3 +1,18 @@
+void smdps_clu11(void);
+
+static void send_mdps_enable_speed(CAN_FIFOMailBox_TypeDef *to_fwd){
+  bool is_speed_unit_mph = GET_BYTE(to_fwd, 2) & 0x2;
+
+  int mdps_cutoff_speed = is_speed_unit_mph ? 76 : 120;  // factor of 2 from dbc
+
+  int veh_clu_speed = GET_BYTE(to_fwd, 1) | (GET_BYTE(to_fwd, 2) & 0x1) << 8;
+
+  if (veh_clu_speed < mdps_cutoff_speed) {
+    to_fwd->RDLR &= 0xFFFE00FF;
+    to_fwd->RDLR |= mdps_cutoff_speed << 8;
+  }
+};
+
 int default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   UNUSED(to_push);
   return true;
@@ -67,6 +82,10 @@ static int default_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
       sunnypilot_detected_last = ts;
     }
     bus_fwd = 2;
+    if (addr == 1265) {
+      send_mdps_enable_speed(to_fwd);
+    }
+    smdps_clu11();
   }
   if (bus_num == 2) {
     // SCC11: Forward radar points to sunnypilot/openpilot
