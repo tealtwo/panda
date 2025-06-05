388905df328af73d34687bd552fe1f5b8e26da9b
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2025-06-05 20:57:10 +0200

    MLB

--- board/safety/safety_volkswagen_mqb.h
+++ board/safety/safety_volkswagen_mqb.h
@@ -16,22 +16,9 @@ const SteeringLimits VOLKSWAGEN_MQB_STEERING_LIMITS = {
 // acceleration in m/s2 * 1000 to avoid floating point math
 const LongitudinalLimits VOLKSWAGEN_MQB_LONG_LIMITS = {
   .max_accel = 2000,
-  .min_accel = -5000,
+  .min_accel = -3500,
   .inactive_accel = 3010,  // VW sends one increment above the max range when inactive
 };
-
-#define MSG_ESP_19      0x0B2   // RX from ABS, for wheel speeds
-#define MSG_LH_EPS_03   0x09F   // RX from EPS, for driver steering torque
-#define MSG_ESP_05      0x106   // RX from ABS, for brake switch state
-#define MSG_TSK_06      0x120   // RX from ECU, for ACC status from drivetrain coordinator
-#define MSG_MOTOR_20    0x121   // RX from ECU, for driver throttle input
-#define MSG_ACC_06      0x122   // TX by OP, ACC control instructions to the drivetrain coordinator
-#define MSG_HCA_01      0x126   // TX by OP, Heading Control Assist steering torque
-#define MSG_GRA_ACC_01  0x12B   // TX by OP, ACC control buttons for cancel/resume
-#define MSG_ACC_07      0x12E   // TX by OP, ACC control instructions to the drivetrain coordinator
-#define MSG_ACC_02      0x30C   // TX by OP, ACC HUD data to the instrument cluster
-#define MSG_MOTOR_14    0x3BE   // RX from ECU, for brake switch status
-#define MSG_LDW_02      0x397   // TX by OP, Lane line recognition and text alerts
 
 // Transmit of GRA_ACC_01 is allowed on bus 0 and 2 to keep compatibility with gateway and camera integration
 const CanMsg VOLKSWAGEN_MQB_STOCK_TX_MSGS[] = {{MSG_HCA_01, 0, 8}, {MSG_GRA_ACC_01, 0, 8}, {MSG_GRA_ACC_01, 2, 8},
@@ -49,50 +36,9 @@ RxCheck volkswagen_mqb_rx_checks[] = {
   {.msg = {{MSG_GRA_ACC_01, 0, 8, .check_checksum = true, .max_counter = 15U, .frequency = 33U}, { 0 }, { 0 }}},
 };
 
-uint8_t volkswagen_crc8_lut_8h2f[256]; // Static lookup table for CRC8 poly 0x2F, aka 8H2F/AUTOSAR
 bool volkswagen_mqb_brake_pedal_switch = false;
 bool volkswagen_mqb_brake_pressure_detected = false;
 
-static uint32_t volkswagen_mqb_get_checksum(const CANPacket_t *to_push) {
-  return (uint8_t)GET_BYTE(to_push, 0);
-}
-
-static uint8_t volkswagen_mqb_get_counter(const CANPacket_t *to_push) {
-  // MQB message counters are consistently found at LSB 8.
-  return (uint8_t)GET_BYTE(to_push, 1) & 0xFU;
-}
-
-static uint32_t volkswagen_mqb_compute_crc(const CANPacket_t *to_push) {
-  int addr = GET_ADDR(to_push);
-  int len = GET_LEN(to_push);
-
-  // This is CRC-8H2F/AUTOSAR with a twist. See the OpenDBC implementation
-  // of this algorithm for a version with explanatory comments.
-
-  uint8_t crc = 0xFFU;
-  for (int i = 1; i < len; i++) {
-    crc ^= (uint8_t)GET_BYTE(to_push, i);
-    crc = volkswagen_crc8_lut_8h2f[crc];
-  }
-
-  uint8_t counter = volkswagen_mqb_get_counter(to_push);
-  if (addr == MSG_LH_EPS_03) {
-    crc ^= (uint8_t[]){0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5}[counter];
-  } else if (addr == MSG_ESP_05) {
-    crc ^= (uint8_t[]){0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07}[counter];
-  } else if (addr == MSG_TSK_06) {
-    crc ^= (uint8_t[]){0xC4,0xE2,0x4F,0xE4,0xF8,0x2F,0x56,0x81,0x9F,0xE5,0x83,0x44,0x05,0x3F,0x97,0xDF}[counter];
-  } else if (addr == MSG_MOTOR_20) {
-    crc ^= (uint8_t[]){0xE9,0x65,0xAE,0x6B,0x7B,0x35,0xE5,0x5F,0x4E,0xC7,0x86,0xA2,0xBB,0xDD,0xEB,0xB4}[counter];
-  } else if (addr == MSG_GRA_ACC_01) {
-    crc ^= (uint8_t[]){0x6A,0x38,0xB4,0x27,0x22,0xEF,0xE1,0xBB,0xF8,0x80,0x84,0x49,0xC7,0x9E,0x1E,0x2B}[counter];
-  } else {
-    // Undefined CAN message, CRC check expected to fail
-  }
-  crc = volkswagen_crc8_lut_8h2f[crc];
-
-  return (uint8_t)(crc ^ 0xFFU);
-}
 
 static safety_config volkswagen_mqb_init(uint16_t param) {
   UNUSED(param);
@@ -162,7 +108,6 @@ static void volkswagen_mqb_rx_hook(const CANPacket_t *to_push) {
         bool resume_button = GET_BIT(to_push, 19U);
         if ((volkswagen_set_button_prev && !set_button) || (volkswagen_resume_button_prev && !resume_button)) {
           controls_allowed = acc_main_on;
-          controls_allowed_long = acc_main_on;
         }
         volkswagen_set_button_prev = set_button;
         volkswagen_resume_button_prev = resume_button;
@@ -170,7 +115,7 @@ static void volkswagen_mqb_rx_hook(const CANPacket_t *to_push) {
       // Always exit controls on rising edge of Cancel
       // Signal: GRA_ACC_01.GRA_Abbrechen
       if (GET_BIT(to_push, 13U)) {
-        controls_allowed_long = false;
+        controls_allowed = false;
       }
     }
 
@@ -242,19 +187,9 @@ static bool volkswagen_mqb_tx_hook(const CANPacket_t *to_send) {
 
   // FORCE CANCEL: ensuring that only the cancel button press is sent when controls are off.
   // This avoids unintended engagements while still allowing resume spam
-  if (addr == MSG_GRA_ACC_01) {
+  if ((addr == MSG_GRA_ACC_01) && !controls_allowed) {
     // disallow resume and set: bits 16 and 19
-    /*if ((GET_BYTE(to_send, 2) & 0x9U) != 0U) {
-      tx = 0;
-    }*/
-    bool is_set_cruise = GET_BIT(to_send, 16U) != 0U;
-    bool is_resume_cruise = GET_BIT(to_send, 19U) != 0U;
-    bool is_accel_cruise = GET_BIT(to_send, 17U) != 0U;
-    bool is_decel_cruise = GET_BIT(to_send, 18U) != 0U;
-    bool is_cancel = GET_BIT(to_send, 13U) != 0U;
-
-    bool allowed = (is_cancel && cruise_engaged_prev) || ((is_set_cruise || is_resume_cruise || is_accel_cruise || is_decel_cruise) && controls_allowed && controls_allowed_long);
-    if (!allowed) {
+    if ((GET_BYTE(to_send, 2) & 0x9U) != 0U) {
       tx = false;
     }
   }
@@ -301,7 +236,7 @@ const safety_hooks volkswagen_mqb_hooks = {
   .rx = volkswagen_mqb_rx_hook,
   .tx = volkswagen_mqb_tx_hook,
   .fwd = volkswagen_mqb_fwd_hook,
-  .get_counter = volkswagen_mqb_get_counter,
-  .get_checksum = volkswagen_mqb_get_checksum,
-  .compute_checksum = volkswagen_mqb_compute_crc,
+  .get_counter = volkswagen_mlb_mqb_get_counter,
+  .get_checksum = volkswagen_mlb_mqb_get_checksum,
+  .compute_checksum = volkswagen_mlb_mqb_compute_crc,
 };
